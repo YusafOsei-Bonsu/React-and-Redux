33c4dea3605076dd0163cfbd74826a5bb4cc0c00
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-10-25 23:56:39 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      
+      <h1>My first React app!</h1>
+      <p>Welcome to my Simple Website, mf! :)</p>
     </div>
   );
 }
