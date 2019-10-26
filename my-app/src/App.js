e8ad289ecd2918f683706f4b598bb4111ff7c602
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-10-26 12:18:18 +0100

    Update App.js
    Made a list of ninjas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,20 @@ import Ninjas from './Ninjas.js';
 
 // App.js is the root component
 class App extends React.Component {
+  state = {
+    ninjas: [
+      {name: 'Yusaf', age: 24, belt: 'White', id: 1},
+      {name: 'Yasser', age: 26, belt: 'Super Black', id: 2},
+      {name: 'Ashish', age: 24, belt: 'Brown', id: 3}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome to my Simple Website, mf! :)</p>
-        <Ninjas name="Yusaf" age="24" belt="White"/>
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     )
   }
