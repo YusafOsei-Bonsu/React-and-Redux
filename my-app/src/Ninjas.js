c3f5c4ab91b1bdceb34d9ed386f6236bd838ff64
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-10-26 16:19:06 +0100

    Update Ninjas.js

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,28 +1,25 @@
 import React from 'react';
 
 // This will be a nested component within the root component (i.e. App.js)
-class Ninjas extends React.Component {
-  render() {
-    const {ninjas} = this.props;
-
-    // Every ninja will be stored in the list
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-          <br/>
-        </div>
-      )
-    })
+const Ninjas = ({ninjas}) => {
+  // Every ninja will be stored in the list
+  const ninjaList = ninjas.map(ninja => {
     return (
-      // Printing each ninja
-      <div className="ninja-list">
-        {ninjaList}
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+        <br/>
       </div>
     )
-  }
+  })
+
+  return (
+    // Printing each ninja
+    <div className="ninja-list">
+      {ninjaList}
+    </div>
+  )
 }
 
 // Export the 'Ninjas' js file
