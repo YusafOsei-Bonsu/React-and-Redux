5c8b82922005146efe687e7d292ed8fb4d06238d
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-10-26 17:39:13 +0100

    Update Ninjas.js
    Made an IF-statement & ternary operator approach for conditional outputs

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,17 +2,34 @@ import React from 'react';
 
 // This will be a nested component within the root component (i.e. App.js)
 const Ninjas = ({ninjas}) => {
-  // Every ninja will be stored in the list
+
   const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ninja.id}>
+  // IF statement approach
+    if (ninja.age >= 10) {
+      return (
+        <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name}</div>
         <div>Age: {ninja.age}</div>
         <div>Belt: {ninja.belt}</div>
         <br/>
-      </div>
-    )
+        </div>
+      )
+    } else {
+      return null;
+    }
   })
+
+  // Ternary operator approach
+  // const ninjaList = ninjas.map(ninja => {
+  //   return ninja.age > 20 ? (
+  //     <div className="ninja" key={ninja.id}>
+  //       <div>Name: {ninja.name}</div>
+  //       <div>Age: {ninja.age}</div>
+  //       <div>Belt: {ninja.belt}</div>
+  //       <br/>
+  //     </div>
+  //   ) : null;
+  // })
 
   return (
     // Printing each ninja
