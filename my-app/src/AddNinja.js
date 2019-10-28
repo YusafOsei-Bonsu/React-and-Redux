784d564171d4ab36427b5b0c02dde0b8569f27c1
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-10-28 12:10:01 +0000

    Update AddNinja.js
    - Edited the state object to one line.
    - For each 'input' html element, the id comes first.

--- my-app/src/AddNinja.js
+++ my-app/src/AddNinja.js
@@ -2,14 +2,11 @@
 import React from 'react'
 
 class AddNinja extends React.Component {
-  state = {
-    name: null,
-    age: null,
-    belt: null
-  }
+  state = {name: null, age: null, belt: null}
 
   handleChange = (e) => {
     this.setState({
+      // Target: The element we're currently on
       [e.target.id]: e.target.value
     })
   }
@@ -26,13 +23,13 @@ class AddNinja extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">Name: </label>
-          <input type="text" id="name" onChange={this.handleChange}/>
+          <input id="name" type="text" onChange={this.handleChange}/>
           <br/>
           <label htmlFor="name">Age: </label>
-          <input type="text" id="age" onChange={this.handleChange}/>
+          <input id="age" type="text" onChange={this.handleChange}/>
           <br/>
           <label htmlFor="name">Belt: </label>
-          <input type="text" id="belt" onChange={this.handleChange}/>
+          <input id="belt" type="text" onChange={this.handleChange}/>
           <br/>
           <button>Submit</button>
         </form>
