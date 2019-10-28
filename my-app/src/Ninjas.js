af4d1bef32d93ceae41c414accdf50ce37556313
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-10-28 14:08:25 +0000

    Update Ninjas.js
    Indented divs and commented out importing code

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import './Ninjas.css'
 
 // This will be a nested component within the root component (i.e. App.js)
 const Ninjas = ({ninjas, deleteNinja}) => {
@@ -8,11 +9,11 @@ const Ninjas = ({ninjas, deleteNinja}) => {
     if (ninja.age >= 10) {
       return (
         <div className="ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div>
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
-        <button onClick={() => {deleteNinja(ninja.id)}}>Delete</button>
-        <br/>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+          <button onClick={() => {deleteNinja(ninja.id)}}>Delete</button>
+          <br/>
         </div>
       )
     } else {
